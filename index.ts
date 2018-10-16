56ed6cdce35609f43b62dafdf4a9c09dcd7332b5
Mike Kitzman <michael.kitzman@mapquest.com>
2018-10-16 15:48:21 -0600

    Added console interactivity

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import * as yargs from 'yargs';
 import { readdirSync, readFileSync } from 'fs';
-import { IChallenge } from './types';
+import { IChallenge, IChallengeInfo, ChallengeAction } from './types';
+const readlineSync = require('readline-sync');
 var marked = require('marked');
 var TerminalRenderer = require('marked-terminal');
 
@@ -13,27 +14,31 @@ const argv = (yargs as any).array('challenges').argv;
 
 const challenges = readdirSync('./challenges');
 
-const {
-  challenge,
-  readme,
-  name 
- } = challenges
+const challengeOptions: IChallengeInfo[] = challenges
   .map(name => {
     const readme = readFileSync(`./challenges/${name}/README.md`, 'utf8');
     const challenge: IChallenge = require(`./challenges/${name}`).challenge;
     return { challenge, readme, name };
-  })
-  .find(payload => {
-    return payload.name === argv.challenge;
   });
-
-if(challenge) {
-  if(argv.v || argv.verbose) {
-    console.log(marked(readme));
-  }
-  console.log(`Executing challenege ${name}`);
-
-  challenge.execute();
-} else {
-  console.log('Unable to find a matching challenege');
+const actionLimit = {
+  limit: input => {
+    return (input || '') === ChallengeAction[ChallengeAction.EXECUTE].toString().toLowerCase() || 
+           (input || '') === ChallengeAction[ChallengeAction.README].toString().toLowerCase()
+  },
+  limitMessage: 'Please type \'execute\' or \'readme\''
 }
+let challengeIndex = -1;
+while((challengeIndex = readlineSync.keyInSelect(challengeOptions.map(option => option.name), 'Please select a challenge to execute.')) >= 0) {
+  const choice : string = readlineSync.question('What would you like to do?  ', actionLimit);
+  
+  switch(ChallengeAction[choice.toUpperCase()]) {
+    case ChallengeAction.EXECUTE:
+      challengeOptions[challengeIndex].challenge.execute()
+      break;
+    case ChallengeAction.README:
+    default:
+      console.log(marked(challengeOptions[challengeIndex].readme));
+      break;
+  }
+}
+console.log('See you later!');
--- types.ts
+++ types.ts
@@ -1,3 +1,13 @@
 export type IChallenge = {
   execute: (args?: any) => void;
 };
+
+export type IChallengeInfo = {
+  name: string,
+  challenge: IChallenge,
+  readme: string;
+};
+
+export enum ChallengeAction {
+  EXECUTE, README
+};
