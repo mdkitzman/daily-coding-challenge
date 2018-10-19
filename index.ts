c5c8d7d8d94565a1dbfef097319774e8b0cd935c
Michael Kitzman <mdkitzman@gmail.com>
2018-10-19 12:10:45 -0600

    slight refactoring of the index script.

--- index.ts
+++ index.ts
@@ -14,31 +14,49 @@ const argv = (yargs as any).array('challenges').argv;
 
 const challenges = readdirSync('./challenges');
 
-const challengeOptions: IChallengeInfo[] = challenges
+const challengeInfos: IChallengeInfo[] = challenges
   .map(name => {
     const readme = readFileSync(`./challenges/${name}/README.md`, 'utf8');
     const challenge: IChallenge = require(`./challenges/${name}`).challenge;
     return { challenge, readme, name };
   });
+
 const actionLimit = {
   limit: input => {
     return (input || '') === ChallengeAction[ChallengeAction.EXECUTE].toString().toLowerCase() || 
            (input || '') === ChallengeAction[ChallengeAction.README].toString().toLowerCase()
   },
   limitMessage: 'Please type \'execute\' or \'readme\''
+};
+const requestNextChallenge = () : IChallengeInfo => {
+  const index = readlineSync.keyInSelect(challengeInfos.map(option => option.name), 'Please select a challenge to execute.');
+  return index > -1 ? 
+    challengeInfos[index] :
+    undefined;
 }
-let challengeIndex = -1;
-while((challengeIndex = readlineSync.keyInSelect(challengeOptions.map(option => option.name), 'Please select a challenge to execute.')) >= 0) {
+const executeChallenge = (challenge: IChallengeInfo) => {
+  challenge.challenge.execute(argv);
+};
+const showReadme = (challenge: IChallengeInfo) => {
+  console.log(marked(challenge.readme));
+};
+
+// Execution loop.
+
+let challengeInfo: IChallengeInfo = undefined;
+while(challengeInfo = requestNextChallenge()) {
+
   const choice : string = readlineSync.question('What would you like to do?  ', actionLimit);
   
   switch(ChallengeAction[choice.toUpperCase()]) {
     case ChallengeAction.EXECUTE:
-      challengeOptions[challengeIndex].challenge.execute()
+      executeChallenge(challengeInfo);
       break;
     case ChallengeAction.README:
     default:
-      console.log(marked(challengeOptions[challengeIndex].readme));
+      showReadme(challengeInfo);
       break;
   }
 }
 console.log('See you later!');
+
