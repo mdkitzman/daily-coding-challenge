39150313c7bcaad7f8962aa39d51b4bac8167939
Mike Kitzman <michael.kitzman@mapquest.com>
2018-11-29 12:41:39 -0700

    Solution sped up, but fails on shuffled big array.

--- challenges/sum-of-numbers/index.ts
+++ challenges/sum-of-numbers/index.ts
@@ -9,46 +9,67 @@ class SumOfNumbersChallenge implements IChallenge {
     [
       {
         sumNumber: 17,
-        array: [10, 15, 3, 7]
+        array: [10, 15, 3, 7],
+        expected: true
       },
       {
         sumNumber: 0,
-        array: [10, 15, 3, 7]
+        array: [10, 15, 3, 7],
+        expected: false
       },
       {
         sumNumber: 0,
-        array: [10, 15, 3, -3]
+        array: [10, 15, 3, -3],
+        expected: true
       },
       {
         sumNumber: -10,
-        array: [10, 15, 3, -13]
+        array: [10, 15, 3, -13],
+        expected: true
       },
       {
         sumNumber: this.maxLarge + this.maxLarge-1,
-        array: Array.from(new Array(this.maxLarge),(val,index)=>index+1)
+        array: shuffle(Array.from(new Array(this.maxLarge),(val,index)=>index+1)),
+        expected: true
       },
       {
         sumNumber: 1,
-        array: []
+        array: [],
+        expected: false
       },
       {
         sumNumber: 1,
-        array: [1]
+        array: [1],
+        expected: false
       }
     ].forEach((item) => {
-      const { array, sumNumber } = item;
-      const mayOrMayNot = sumExists(sumNumber, array) ?
-        'does' :
-        'does not';
+      const { array, sumNumber, expected } = item;
       const arrayStr = array.length < 5 ?
         array.toString() :
         `${array[0]}, ${array[1]}, ... , ${array[array.length-2]}, ${array[array.length-1]}`;
-      console.log(`The array [${arrayStr}] ${mayOrMayNot} contain the sum number of ${sumNumber}`);
+
+      const msg = `sumExists of array length ${array.length} took`;
+
+      console.time(msg)
+      const exists = sumExists(sumNumber, array);
+      console.timeEnd(msg);
+
+      console.log(`${expected === exists ?'✔':'X'} - The array [${arrayStr}] ${exists?'does':'does not'} contain the sum number of ${sumNumber}\n`);
     });
     
   }
 
 }
+
+// Thanks SO!  - https://stackoverflow.com/a/12646864
+function shuffle(array: any[]) : any[]{
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
 
 export const challenge: IChallenge = new SumOfNumbersChallenge();
 
